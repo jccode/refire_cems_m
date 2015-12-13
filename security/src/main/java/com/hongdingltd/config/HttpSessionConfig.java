e4fcf31254d75515d11cd3826f62964dbf637047
junchangchen <junchangchen@gmail.com>
2015-12-13 12:17:06 +0800

    spring session

---
+++ security/src/main/java/com/hongdingltd/config/HttpSessionConfig.java
@@ -0,0 +1,10 @@
+package com.hongdingltd.config;
+
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+/**
+ * Created by jcchen on 15-12-12.
+ */
+@EnableRedisHttpSession
+public class HttpSessionConfig {
+}
